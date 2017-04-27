a1e04107f6cc9cca019e5529b28b0b687f52d133
vuleenguyen <vuleenguyen.92@gmail.com>
2017-04-27 04:57:24 -0700

    refactor code

--- WebStore/src/main/java/com/mum/edu/config/DBconnection.java
+++ WebStore/src/main/java/com/mum/edu/config/DBconnection.java
@@ -10,10 +10,9 @@ public class DBconnection {
 
     private static final String connectionURL = "jdbc:mysql://localhost:80/webstoredb";
     
- // Kết nối vào MySQL.
     public static synchronized Connection getMySQLConnection() throws SQLException,
             ClassNotFoundException {
-        return getMySQLConnection(connectionURL, "root", "");
+        return getMySQLConnection(connectionURL, "root", "1234");
     }
     
     public static synchronized Connection getMySQLConnection(String connectionURL,
@@ -25,40 +24,4 @@ public class DBconnection {
                 password);
         return conn;
     }
-
-    String retrieveUserFullname(String email) throws ClassNotFoundException {
-        String readQuery = "SELECT fullname from users where email = '" + email + "';";
-        String fullname = "No information found for the requested user: " + email;
-
-        try (Connection con = DBconnection.getMySQLConnection();
-                Statement stmt = con.createStatement();) {
-            System.out.println("the query: " + readQuery);
-            ResultSet rs = stmt.executeQuery(readQuery);
-            while (rs.next()) {
-                fullname = rs.getString("fullname");
-                System.out.println("User Fullname: " + fullname);
-            }
-            stmt.close();
-
-        } catch (SQLException s) {
-            System.out.println("Exception thrown in retrieveUser ....");
-            s.printStackTrace();
-        }
-
-        return fullname;
-
-    }
-
-    public String mockRetrieveUserFullname(String email) {
-        String fullname = "no definition found";
-        switch (email) {
-            case "asaad@mum.edu":
-                fullname = "Asaad Saad";
-                break;
-            default: fullname = "CS472 Student";
-                break;
-        }
-        return fullname;
-    }
-
 }
